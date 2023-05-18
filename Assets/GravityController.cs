5fb401d0f4d2e2f9732b4eb3fedee661810234d8
Iqbalmubarok24 <mubarokiqbal362@gmail.com>
2023-05-18 15:53:46 +0700

    Fix

--- Assets/GravityController.cs
+++ Assets/GravityController.cs
@@ -6,21 +6,35 @@ public class GravityController : MonoBehaviour
 {
     [SerializeField] float acceleration = 9.8f;
 
+    Vector3 gravityOffset = Vector3.zero;
+
     void Start()
     {
         if (SystemInfo.supportsGyroscope)
             Input.gyro.enabled = true;
+
+        CalibrateGravity();
     }
 
     void Update()
+    {
+        Physics.gravity = GetGravityFromSensor() + gravityOffset;
+    }
+
+    public void CalibrateGravity()
+    {
+        gravityOffset = Vector3.down * acceleration - GetGravityFromSensor();
+    }
+
+    public Vector3 GetGravityFromSensor()
     {
         Vector3 gravity;
         if (Input.gyro.gravity != Vector3.zero)
             gravity = Input.gyro.gravity * acceleration;
         else
-            gravity = Input.acceleration * acceleration;
+            gravity = Input.gyro.gravity * acceleration;
 
-        gravity.z = Mathf.Clamp(gravity.z, float.MinValue,-1);
-        Physics.gravity = new Vector3(gravity.x,gravity.z,gravity.y);
+        gravity.z = Mathf.Clamp(gravity.z,float.MinValue,1);
+        return new Vector3(gravity.x,gravity.z,gravity.y);
     }
 }
